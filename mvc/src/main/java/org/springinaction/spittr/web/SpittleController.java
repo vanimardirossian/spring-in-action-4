9bd814e2d7d875cd3e64ef000ca7da26739e8f88
Vani Mardirossian <vani.mardirossian@gmail.com>
2022-09-09 21:09:38 +0400

    sia chapter 5 - mvc

---
+++ mvc/src/main/java/org/springinaction/spittr/web/SpittleController.java
@@ -0,0 +1,9 @@
+package org.springinaction.spittr.web;
+
+import org.springinaction.spittr.data.SpittleRepository;
+
+public class SpittleController {
+    public SpittleController(SpittleRepository spittleRepository) {
+
+    }
+}
